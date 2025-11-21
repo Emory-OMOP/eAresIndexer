e356c1612b29f53098d5dbe8336c27f398017d76
Jorge M. Marquez <jorge.m.marquez@emory.edu>
2025-11-21 13:44:37 -0500

    Add normalization for query results in getSourceReleaseKey function and error handling

--- R/GetSourceReleaseKey.R
+++ R/GetSourceReleaseKey.R
@@ -9,6 +9,15 @@ getSourceReleaseKey <- function(connectionDetails, cdmDatabaseSchema) {
 
   connection <- DatabaseConnector::connect(connectionDetails)
   results <- DatabaseConnector::querySql(connection = connection, sql = translatedRenderedSql)
+  
+  # Normalize column names to uppercase to be resilient to Redshift/Postgres lowercase returns
+  if (!is.null(results)) {
+    names(results) <- toupper(names(results))
+  } else {
+    warning("Query to cdm_source returned NULL. Unable to generate source release key.")
+    return(NULL)
+  }
+  
   releaseId <- format(lubridate::ymd(results[1,"CDM_RELEASE_DATE"]),"%Y%m%d")
   sourceKey <- gsub(" ", "_", results[1,"CDM_SOURCE_ABBREVIATION"])
 
